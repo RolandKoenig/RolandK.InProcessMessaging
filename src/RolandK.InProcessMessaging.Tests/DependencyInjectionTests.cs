ca174bc7b1d24bdd4e897b4be4f862cd68a70b49
Roland König <roland.koenig@rolandk.de>
2023-02-06 22:07:26 +0100

    Updated README.md, added tests

---
+++ src/RolandK.InProcessMessaging.Tests/DependencyInjectionTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RolandK.InProcessMessaging.Tests;
+
+public class DependencyInjectionTests
+{
+    [Fact]
+    public void Publish_Subscribe_using_DependencyInjection()
+    {
+        // Arrange
+        var messenger = new InProcessMessenger();
+
+        var services = new ServiceCollection();
+        services.AddSingleton<IInProcessMessagePublisher>(messenger);
+        services.AddSingleton<IInProcessMessageSubscriber>(messenger);
+
+        using var serviceProvider = services.BuildServiceProvider();
+
+        // Act
+        var countReceivedMessages = 0;
+        
+        var subscriber = serviceProvider.GetRequiredService<IInProcessMessageSubscriber>();
+        using var subscription = subscriber.Subscribe<DummyMessage>(_ => countReceivedMessages++);
+
+        var publisher = serviceProvider.GetRequiredService<IInProcessMessagePublisher>();
+        publisher.Publish<DummyMessage>();
+
+        // Assert
+        Assert.Equal(1, countReceivedMessages);
+    }
+    
+    //*************************************************************************
+    //*************************************************************************
+    //*************************************************************************
+    [InProcessMessage]
+    private record DummyMessage;
+}
